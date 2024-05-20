843009248456e25af8c288af35b76482df8cc9c1
Pradip Nikam <nikampradip863@gmail.com>
2024-05-20 23:25:29 +0530

    Comment added

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -9,6 +9,7 @@ const Popular = () => {
     <h1>POPULAR IN WOMEN</h1>
       <hr />
       <div className="popular-item">
+      {/* Nothing */}
         {data_product.map((item, i)=>{
           return <Item Key={i} id={item.id} name={item.name} image={item.image} 
           new_price={item.new_price} old_price={item.old_price} />
